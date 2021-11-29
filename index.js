1fb708b877e9390522535766f164712457bba967
Christine Seddon <noddes@gmail.com>
2021-11-28 18:38:34 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// const batteryBatches = [4, 5, 3, 4, 4, 6, 5].reduce(totalBatteries, 0);
+
+// function totalBatteries(accumulator, a) {
+    
+//     return accumulator + a;
+//     }
+
+// console.log(batteryBatches);
+
+const totalBatteries = [4, 5, 3, 4, 4, 6, 5].reduce(batteryBatches, 0);
+
+function batteryBatches(accumulator, a) {
+    
+    return accumulator + a;
+    }
+
+console.log(totalBatteries);
